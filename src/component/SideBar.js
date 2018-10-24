f419544fe6597d39884aa5d16d6b38667ea15fc6
Alex Morales <a.jrmorales@gmail.com>
2018-10-24 11:21:37 -0700

    Search bar is console logging query.

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -2,10 +2,30 @@ import React, {Component} from "react";
 import VenueList from "./VenueList"
 
 export default class SideBar extends Component {
+
+  constructor(props) {
+    super(props)
+    //DON'T CALL this.setState() here!
+    this.state = {
+      query: ''
+    }
+  }
+  
+  inputChange = (event) => {
+    const query = event.target.value;
+    this.setState({query});
+    return console.log(query)
+  }
+
   render() {
     return (
       <div className="sideBar">
-        <input type={"search"} id={"search"} placeholder={"Filter venues"} />
+        <input
+          type={"search"}
+          id={"search"}
+          placeholder={"Filter venues"}
+          onChange={this.inputChange}
+        />
         <VenueList {...this.props}
         handleListItemClick={this.props.handleListItemClick}
         />
